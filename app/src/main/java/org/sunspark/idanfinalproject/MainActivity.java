38538138ebe73137526a25d66ccaef450c751867
Idan <idan.mtv@gmail.com>
2020-06-18 19:19:07 +0200

    add a few buttons

--- app/src/main/java/org/sunspark/idanfinalproject/MainActivity.java
+++ app/src/main/java/org/sunspark/idanfinalproject/MainActivity.java
@@ -2,13 +2,26 @@ package org.sunspark.idanfinalproject;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
+    private Button btn1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        btn1 = (Button) findViewById(R.id.btn1);
+        btn1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent myIntent = new Intent(MainActivity.this, Main2Activity.class);
+                myIntent.putExtra("key", button);
+                MainActivity.this.startActivity(myIntent);
+
+            }
+        });
     }
 }
